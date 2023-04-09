e48887124d14af57f523a5153183aa0453d56651
SachinMudannayaka <122568286+SachinMudannayaka@users.noreply.github.com>
2023-04-09 22:46:38 +0530

    Checked templateURL)

---
+++ src/app/main/main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  template: `<h1>Sachin is a third year student at SLIIT</h1><br><h2>You should work hard if you want to success in one day</h2>`,
+  styleUrls: ['./main.component.css']
+})
+export class MainComponent {
+
+}
